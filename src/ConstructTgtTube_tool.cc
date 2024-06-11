a75155a7f49eb8d991b5f166b432abd9dce320c3
Michael Hedges <hedges7@purdue.edu>
2024-06-11 12:46:31 -0500

    add prodtgttube example

---
+++ src/ConstructTgtTube_tool.cc
@@ -0,0 +1,118 @@
+//
+// Free function to create a geant4 test environment geometry
+//
+//
+// Original author KLG
+//
+// Notes:
+//
+// one can nest volume inside other volumes if needed
+// see other construct... functions for examples
+//
+
+#include "art/Utilities/ToolMacros.h"
+#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
+
+// Mu2e includes.
+
+#include "Offline/Mu2eG4/inc/MaterialFinder.hh"
+#include "Offline/Mu2eG4/inc/InitEnvToolBase.hh"
+#include "Offline/Mu2eG4/inc/nestTubs.hh"
+#include "Offline/Mu2eG4/inc/nestBox.hh"
+#include "Offline/Mu2eG4Helper/inc/VolumeInfo.hh"
+#include "Offline/ConfigTools/inc/SimpleConfig.hh"
+
+// G4 includes
+#include "Geant4/G4ThreeVector.hh"
+#include "Geant4/G4Material.hh"
+#include "Geant4/G4Color.hh"
+#include "Geant4/G4Tubs.hh"
+#include "Geant4/G4RotationMatrix.hh"
+#include "Offline/Mu2eG4Helper/inc/Mu2eG4Helper.hh"
+#include "CLHEP/Units/SystemOfUnits.h"
+
+using namespace std;
+
+namespace mu2e {
+
+  class ConstructTgtTube: public InitEnvToolBase {
+  public:
+    ConstructTgtTube(const fhicl::ParameterSet& PSet) {}
+    int construct(VolumeInfo const& ParentVInfo, SimpleConfig const& Config);
+  };
+
+//-----------------------------------------------------------------------------
+  int ConstructTgtTube::construct(VolumeInfo const& parentVInfo, SimpleConfig const& _config) {
+
+    const bool forceAuxEdgeVisible = _config.getBool("g4.forceAuxEdgeVisible");
+    const bool doSurfaceCheck      = _config.getBool("g4.doSurfaceCheck");
+    const bool placePV             = true;
+
+    // Extract box information from the config file.
+
+    G4bool boxVisible        = _config.getBool("box.visible",true);
+    G4bool boxSolid          = _config.getBool("box.solid",true);
+
+    vector<double> boxParams;
+    _config.getVectorDouble( "box.halfLengths", boxParams);
+
+    MaterialFinder materialFinder(_config);
+    G4Material* boxMaterial = materialFinder.get("box.wallMaterialName");
+
+    const G4ThreeVector boxCenterInWorld(_config.getHep3Vector("box.centerInWorld"));
+
+    G4Colour  orange  (.75, .55, .0);
+
+    VolumeInfo boxVInfo(nestBox( "Box",
+                                 boxParams,
+                                 boxMaterial,
+                                 0, // no rotation
+                                 boxCenterInWorld,
+                                 parentVInfo,
+                                 _config.getInt("box.copyNumber",2),
+                                 // we assign a non 0 copy nuber for
+                                 // volume tracking purposes
+                                 boxVisible,
+                                 orange,
+                                 boxSolid,
+                                 forceAuxEdgeVisible,
+                                 placePV,
+                                 doSurfaceCheck
+                                 ));
+
+    // Extract tube information from the config file.
+
+    G4bool tubeVisible        = _config.getBool("tube.visible",true);
+    G4bool tubeSolid          = _config.getBool("tube.solid",true);
+
+
+    TubsParams tubeParams( _config.getDouble("tube.rIn"),
+                           _config.getDouble("tube.rOut"),
+                           _config.getDouble("tube.halfLength"));
+
+    G4Material* tubeMaterial = materialFinder.get("tube.wallMaterialName");
+
+    const G4ThreeVector tubeCenterInWorld(_config.getHep3Vector("tube.centerInWorld"));
+
+    VolumeInfo tubeVInfo(nestTubs( "Tube",
+                                   tubeParams,
+                                   tubeMaterial,
+                                   0, // no rotation
+                                   tubeCenterInWorld,
+                                   parentVInfo,
+                                   _config.getInt("tube.copyNumber",3),
+                                   // we assign a non 0 copy nuber for
+                                   // volume tracking purposes
+                                   tubeVisible,
+                                   orange,
+                                   tubeSolid,
+                                   forceAuxEdgeVisible,
+                                   placePV,
+                                   doSurfaceCheck
+                                   ));
+    return 0;
+  }
+
+}
+
+DEFINE_ART_CLASS_TOOL(mu2e::ConstructTgtTube)
